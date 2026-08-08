e64d5f8d641856fa09f3ddd58c4c6431eef3b6ce
Rithvik <rithvikreddym2006@gmail.com>
2026-08-08 15:18:28 +0530

    Stage 8 Implemented

---
+++ Stage 8/build.sh
@@ -0,0 +1,10 @@
+gcc -g -o xps \
+    main.c \
+    core/xps_core.c \
+    core/xps_loop.c \
+    lib/vec/vec.c \
+    network/xps_connection.c \
+    network/xps_listener.c \
+    utils/xps_logger.c \
+    utils/xps_utils.c \
+    utils/xps_buffer.c
